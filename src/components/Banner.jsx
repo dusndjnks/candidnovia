5729da425a7bf655e01a0904d7a410e2285abcd9
dusndjnks <mhdaffan0000@gmail.com>
2025-04-03 16:35:36 +0530

    x

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,7 +1,7 @@
 const Banner = () => {
     return (
       <div className="relative w-full h-screen">
-        <img src="https://lh3.googleusercontent.com/p/AF1QipNdF_MUxpL_1T2f39-09cMLmkjz_BuGa13FFuFS=s870-k-no" alt="" className="absolute inset-0 w-full h-full object-cover"/>        
+        <img src="https://lh3.googleusercontent.com/p/AF1QipNdF_MUxpL_1T2f39-09cMLmkjz_BuGa13FFuFS=s870-k-no" alt="" className="absolute inset-0 w-full sm:h-full h-[400px] object-cover"/>        
         <div className="absolute inset-0 bg-black bg-opacity-40"></div>
         <div className="relative z-10 flex flex-col items-center justify-center h-full text-white text-center px-4">
           <h1 className="text-4xl sm:text-6xl font-bold mb-4">Welcome to Our Website</h1>
